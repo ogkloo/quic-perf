512122012bc44a0b8e80e244678c7d810d1e2256
Sam <weathercontrol@tuta.io>
2024-04-09 11:56:31 -0700

    Add different versions of binary and dependencies.

---
+++ src/tcp_throughput.rs
@@ -0,0 +1,10 @@
+use std::net::{TcpListener, TcpStream};
+
+async fn test_client(stream: TcpStream) {
+    unimplemented!("Test client not implemented")
+}
+
+#[tokio::main]
+async fn main() {
+    println!("Hello, world!");
+}
