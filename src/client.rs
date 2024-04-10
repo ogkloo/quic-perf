732d45ceabaa37b84f88296c22d2492c5e148a34
ogkloo <weathercontrol@tuta.io>
2024-04-10 10:13:41 -0700

    Add command line interface.

--- src/client.rs
+++ src/client.rs
@@ -1,18 +1,39 @@
-use std::env;
 use std::io::prelude::*;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpStream};
 use std::str::FromStr;
 use std::time::{Duration, Instant};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Address of quic-perf server.
+    #[arg(short, long)]
+    connection_addr: String,
+
+    /// Port of quic-perf server.
+    #[arg(short, long, default_value_t = 5201)]
+    port: u16,
+
+    /// Size of buffer to send in bytes.
+    #[arg(short, long, default_value_t = 1024)]
+    bufsize: usize
+}
 
 /// Client for all Rust versions.
 fn main() -> std::io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    let server_addr: &str = &args[1];
-    let server_port: u16 = args[2].parse::<u16>().unwrap();
-    let bufsize: usize = args[3].parse::<usize>().unwrap();
+    let cli = Args::parse();
+    let server_addr = cli.connection_addr;
+    let server_port = cli.port;
+    let bufsize = cli.bufsize;
+    
+    // let args: Vec<String> = env::args().collect();
+    // let server_addr: &str = &args[1];
+    // let server_port: u16 = args[2].parse::<u16>().unwrap();
+    // let bufsize: usize = args[3].parse::<usize>().unwrap();
 
     let sk_addr = SocketAddr::new(
-        IpAddr::V4(Ipv4Addr::from_str(server_addr).unwrap()),
+        IpAddr::V4(Ipv4Addr::from_str(&server_addr).unwrap()),
         server_port,
     );
 
