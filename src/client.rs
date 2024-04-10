0de0de2fa3f466bb2324173f362ff15672104d09
Sam <weathercontrol@tuta.io>
2024-04-10 12:19:48 -0700

    Change short arguments

--- src/client.rs
+++ src/client.rs
@@ -1,21 +1,23 @@
+use clap::Parser;
 use std::io::prelude::*;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpStream};
 use std::str::FromStr;
 use std::time::{Duration, Instant};
-use clap::Parser;
 
 #[derive(Parser, Debug)]
-#[command(version, 
-          about = "Client for quic-perf.",
-          long_about = "An iperf-like tool for measuring 
-          performance across quic backends implemented in Rust.")]
+#[command(
+    version,
+    about = "Client for quic-perf.",
+    long_about = "An iperf-like tool for measuring 
+          performance across quic backends implemented in Rust."
+)]
 struct Args {
     /// Address of quic-perf server.
-    #[arg(short='c', long="server")]
+    #[arg(short = 'c', long = "server")]
     connection_addr: String,
 
     /// Port of quic-perf server.
-    #[arg(short, long, default_value_t = 5201)]
+    #[arg(short = 'p', long, default_value_t = 5201)]
     port: u16,
 
     /// Size of buffer to attempt to send in bytes.
@@ -28,20 +30,20 @@ struct Args {
 
     /// Ouput format in order of magnitude.
     ///
-    /// Output format in order of magnitude. Use capital letters for values 
+    /// Output format in order of magnitude. Use capital letters for values
     /// in bits, lowercase for values in bytes. E.g. M for megabits per second,
     /// m for megabytes per second.
     #[arg(short, long, default_value_t = 'M')]
     format: char,
 
     /// Precision of rate output.
-    #[arg(short, long, default_value_t = 2)]
+    #[arg(short = 'r', long, default_value_t = 2)]
     precision: usize,
 
-    /// Maximum transmission to send in an interval. Warning: Broken, does 
+    /// Maximum transmission to send in an interval. Warning: Broken, does
     /// not give accurate rate estimate.
     ///
-    /// Maximum transmission to send in an interval. By default there is no 
+    /// Maximum transmission to send in an interval. By default there is no
     /// limit, it will actually saturate the link. This can be an issue if you
     /// are for instance on a public or metered wireless connection.
     ///
@@ -66,7 +68,7 @@ fn main() -> std::io::Result<()> {
         'k' => 8_192,
         'm' => 8_388_608,
         'g' => 8_589_934_592,
-        e => panic!("Invalid format argument: {:?}", e)
+        e => panic!("Invalid format argument: {:?}", e),
     };
     let format_string = match cli.format {
         // Bits
@@ -78,11 +80,11 @@ fn main() -> std::io::Result<()> {
         'm' => "MB",
         'g' => "GB",
         // Unreachable
-        _ => ""
+        _ => "",
     };
     let precision = cli.precision;
     let max_transmission = cli.max_transmission;
-    
+
     let sk_addr = SocketAddr::new(
         IpAddr::V4(Ipv4Addr::from_str(&server_addr).unwrap()),
         server_port,
@@ -103,42 +105,47 @@ fn main() -> std::io::Result<()> {
             stream.read_exact(&mut recv_buf)?;
             buffers_sent += 1;
             if let Some(m_tr) = max_transmission {
-                if m_tr < (buffers_sent*bufsize) {
+                if m_tr < (buffers_sent * bufsize) {
                     let time_spent = now.elapsed().as_secs_f64();
-                    approx_rate = (buffers_sent*bufsize) as f64/time_spent;
+                    approx_rate = (buffers_sent * bufsize) as f64 / time_spent;
                     println!("{}", time_spent);
                     break;
                 }
             }
         }
 
-        if let Some(m_tr) = max_transmission {
-            println!("{} \t\t {}{} \t {:.*}{}/s", 
-                     t+1,
-                     (buffers_sent * 8 * bufsize)/format, 
-                     format_string,
-                     precision, 
-                     approx_rate / format as f64, 
-                     format_string);
-        }
-        else {
-            println!("{} \t\t {:.*}{} \t {:.*}{}/s", 
-                     t+1,
-                     precision,
-                     (buffers_sent * 8 * bufsize) as f64/format as f64, 
-                     format_string,
-                     precision, 
-                     (buffers_sent as f64 * 8.0 * bufsize as f64) / format as f64, 
-                     format_string);
+        if let Some(_) = max_transmission {
+            println!(
+                "{} \t\t {}{} \t {:.*}{}/s",
+                t + 1,
+                (buffers_sent * 8 * bufsize) / format,
+                format_string,
+                precision,
+                approx_rate / format as f64,
+                format_string
+            );
+        } else {
+            println!(
+                "{} \t\t {:.*}{} \t {:.*}{}/s",
+                t + 1,
+                precision,
+                (buffers_sent * 8 * bufsize) as f64 / format as f64,
+                format_string,
+                precision,
+                (buffers_sent as f64 * 8.0 * bufsize as f64) / format as f64,
+                format_string
+            );
         }
         total_buffers_sent += buffers_sent;
     }
 
-    println!("Average rate over {} seconds: {:.*}{}", 
-             time,
-             precision,
-             (total_buffers_sent as f64 * 8.0 * bufsize as f64) / (format*time) as f64, 
-             format_string);
+    println!(
+        "Average rate over {} seconds: {:.*}{}",
+        time,
+        precision,
+        (total_buffers_sent as f64 * 8.0 * bufsize as f64) / (format * time) as f64,
+        format_string
+    );
 
     Ok(())
 }
